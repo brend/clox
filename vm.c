b2b1e1b6ac3ba9ccc3400ce89daf52563b996e3c
Philipp Brendel <waldrumpus@gmail.com>
2026-09-06 20:17:52 +0200

    arithmetic operations

--- vm.c
+++ vm.c
@@ -19,6 +19,12 @@ void freeVM() {}
 static InterpretResult run() {
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+#define BINARY_OP(op) \
+    do { \
+        double b = pop(); \
+        double a = pop(); \
+        push(a op b); \
+    } while (false);
 
     for (;;) {
 #ifdef DEBUG_TRACE_EXECUTION
@@ -38,7 +44,11 @@ static InterpretResult run() {
                 push(constant);
                 break;
             }
-            case OP_NEGATE: push(-pop()); break;
+            case OP_ADD:        BINARY_OP(+); break;
+            case OP_SUBTRACT:   BINARY_OP(-); break;
+            case OP_MULTIPLY:   BINARY_OP(*); break;
+            case OP_DIVIDE:     BINARY_OP(/); break;
+            case OP_NEGATE:     push(-pop()); break;
             case OP_RETURN: {
                 printValue(pop());
                 printf("\n");
@@ -48,6 +58,7 @@ static InterpretResult run() {
     }
 #undef READ_BYTE
 #undef READ_CONSTANT
+#undef BINARY_OP
 }
 
 InterpretResult interpret(Chunk *chunk) {
